a0abc40e5dd2b747cf2f0765c89ec1cbe1642140
Consiliarius <78850500+Consiliarius@users.noreply.github.com>
2022-07-08 21:43:52 +0100

    new version

--- code.py
+++ code.py
@@ -1,18 +1,44 @@
-# Radar callable by function
+# Sonar callable by function
+# Activated by button
 # by Consiliarius
 
-"""
-STARTUP SECTION: Runs once
-"""
 
 # Import required modules
 import board
 import pulseio
 import time
+import neopixel
+import simpleio
+"""
+STARTUP SECTION: Runs once
+"""
+
+# Initialize Neopixel RGB LEDs
+def set-rgb-leds:
+    pixels = neopixel.NeoPixel(board.GP18, 2)
+    pixels.fill(0)
+
+color = 0
+state = 0
+
+# Define pin connected to piezo buzzer and pick some tunes
+def set-buzzer-n-tones:
+    PIEZO_PIN = board.GP22
+
+# Initialize buttons
+def set-buttons:
+    btn20 = digitalio.DigitalInOut(board.GP20)
+    btn21 = digitalio.DigitalInOut(board.GP21)
+    btn20.direction = digitalio.Direction.INPUT
+    btn21.direction = digitalio.Direction.INPUT
+    btn20.pull = digitalio.Pull.UP
+    btn21.pull = digitalio.Pull.UP
+
+
 
 # Create the class to control the sensor
 class GroveUltrasonicTransceiver(object):
-    # At initialisation, bind a pin (GP1) to receive digital pulses, and store one pulse at a time as variable
+    # At initialisation, bind a pin (GP28) to receive digital pulses, and store one pulse at a time as variable
     def __init__(self):
         self.listen =pulseio.PulseIn(board.GP1,maxlen=1)
     
@@ -49,7 +75,7 @@ class GroveUltrasonicTransceiver(object):
                 return dist
 
 # Call the transceiver class
-GUT = GroveUltrasonicTransceiver()
+Sonar = GroveUltrasonicTransceiver()
 
 
 """
@@ -57,11 +83,34 @@ RUN LOOP: Runs indefinitely
 """
 
 while True:
-    # Check the transceiver is sending realistic values back (max range ~350-400cm)...
-    if GUT.get_distance() < 400:
-        print('Surface is {} cm away'.format(GUT.get_distance()))
-    # ...and alert if value can't be trusted
-    else:
-        print('Surface is over 4m away')
-    # Wait a moment, then go again
-    time.sleep(2.5)
+    
+    # Check button 1 (GP20)
+    if not btn20.value:  # button 1 pressed 
+        
+        # Check the transceiver is sending realistic values back (max range ~350-400cm)...
+        if GUT.get_distance() < 400:
+            
+            # ...if it is, signal with a tone & light up LEDS green...
+            pixels.fill(11,102,36)
+            simpleio.tone(PIEZO_PIN, 600, duration=0.1)
+            time.sleep(0.2)
+            simpleio.tone(PIEZO_PIN, 600, duration=0.8)
+                       
+            # and print distance to the console
+            print('Surface is {} cm away'.format(Sonar.get_distance()))
+            time.sleep(1)
+       
+        # ...and alert if value can't be trusted
+        else:
+            pixels.fill(255,0,0)
+            simpleio.tone(PIEZO_PIN, 262, duration=0.5)
+            
+            print('Surface is over 4m away')
+            time.sleep(1)
+            
+            
+    elif not btn21.value:
+
+# reset colour and debounce
+    pixels.fill(0)
+    time.sleep(0.05)
